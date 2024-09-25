1a5cdd0fc5fb7754effe4fabfb92c3083334676e
SasoriAka96 <nahivallejo46@gmail.com>
2024-09-25 17:18:42 +0200

    visual añadido

--- CalculatorCompose/app/src/main/java/com/example/calculatorcompose/MainActivity.kt
+++ CalculatorCompose/app/src/main/java/com/example/calculatorcompose/MainActivity.kt
@@ -11,13 +11,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -54,7 +56,7 @@ fun Calculadora() {
                 TextField(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = 16.dp)
+                        .padding(horizontal = 11.dp)
                         .border(width = 1.dp, color = Color.DarkGray),
                     value = "",
                     onValueChange = {},
@@ -65,96 +67,198 @@ fun Calculadora() {
 
         }
         Column(
-            Modifier
-                .background(color = Color.Red)
-                .align(alignment = Alignment.CenterHorizontally),
+            modifier = Modifier.fillMaxWidth().height(500.dp),
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
 
-            )
-        {
-
-            Row {
-                Button(onClick = {
-                }) {
-
+            Row(Modifier.padding(start = 10.dp)) {
+                Button(
+                    onClick = {},
+                    Modifier
+                        .height(71.dp)
+                        .width(90.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(4283848278))
+                ) {
                     Text(text = "AC")
                 }
             }
-            Row {
-                Button(onClick = {
-                }) {
+            Row(modifier = Modifier.fillMaxWidth(),horizontalArrangement = Arrangement.SpaceAround) {
+                Button(
+                    onClick = {},
+                    Modifier
+                        .height(71.dp)
+                        .width(90.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(4281545523)),
+
+                    ) {
                     Text(text = "7")
                 }
-                Button(onClick = {
-                }) {
+                Button(
+                    onClick = {},
+                    Modifier
+                        .height(71.dp)
+                        .width(90.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(4281545523))
+                ) {
                     Text(text = "8")
                 }
-                Button(onClick = {
-                }) {
+                Button(
+                    onClick = {},
+                    Modifier
+                        .height(71.dp)
+                        .width(90.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(4281545523))
+                ) {
                     Text(text = "9")
                 }
-                Button(onClick = {
-                }) {
+                Button(
+                    onClick = {},
+                    Modifier
+                        .height(71.dp)
+                        .width(90.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(4279402906))
+                ) {
                     Text(text = "x")
                 }
 
             }
-            Row {
-                Button(onClick = {
-                }) {
+            Row (modifier = Modifier.fillMaxWidth(),horizontalArrangement = Arrangement.SpaceAround) {
+                Button(
+                    onClick = {},
+                    Modifier
+                        .height(71.dp)
+                        .width(90.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(4281545523))
+                ) {
                     Text(text = "4")
                 }
-                Button(onClick = {
-                }) {
+                Button(
+                    onClick = {},
+                    Modifier
+                        .height(71.dp)
+                        .width(90.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(4281545523))
+                ) {
                     Text(text = "5")
                 }
-                Button(onClick = {
-                }) {
+                Button(
+                    onClick = {},
+                    Modifier
+                        .height(71.dp)
+                        .width(90.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(4281545523))
+                ) {
                     Text(text = "6")
                 }
-                Button(onClick = {
-                }) {
+                Button(
+                    onClick = {},
+                    Modifier
+                        .height(71.dp)
+                        .width(90.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(4279402906))
+                ) {
                     Text(text = "-")
                 }
             }
-            Row {
-                Button(onClick = {
-                }) {
+            Row (modifier = Modifier.fillMaxWidth(),horizontalArrangement = Arrangement.SpaceAround){
+                Button(
+                    onClick = {},
+                    Modifier
+                        .height(71.dp)
+                        .width(90.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(4281545523))
+                ) {
                     Text(text = "1")
                 }
-                Button(onClick = {
-                }) {
+                Button(
+                    onClick = {},
+                    Modifier
+                        .height(71.dp)
+                        .width(90.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(4281545523))
+                ) {
                     Text(text = "2")
                 }
-                Button(onClick = {
-                }) {
+                Button(
+                    onClick = {},
+                    Modifier
+                        .height(71.dp)
+                        .width(90.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(4281545523))
+                ) {
                     Text(text = "3")
                 }
-                Button(onClick = {
-                }) {
+                Button(
+                    onClick = {},
+                    Modifier
+                        .height(71.dp)
+                        .width(90.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(4279402906)
+                    )
+                ) {
                     Text(text = "+")
                 }
 
             }
-            Row {
-                Button(onClick = {
-                }) {
+            Row (modifier = Modifier.fillMaxWidth(),horizontalArrangement = Arrangement.SpaceAround){
+                Button(
+                    onClick = {},
+                    Modifier
+                        .height(71.dp)
+                        .width(90.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(4281545523))
+                ) {
                     Text(text = "0")
                 }
-                Button(onClick = {
-                }) {
+                Button(
+                    onClick = {},
+                    Modifier
+                        .height(71.dp)
+                        .width(90.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(4281545523))
+                ) {
                     Text(text = ",")
                 }
-                Button(onClick = {
-                }) {
+                Button(
+                    onClick = {},
+                    Modifier
+                        .height(71.dp)
+                        .width(90.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(4281545523))
+                ) {
                     Text(text = "/")
                 }
-                Button(onClick = {
-                }) {
+                Button(
+                    onClick = {},
+                    Modifier
+                        .height(71.dp)
+                        .width(90.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(4279402906))
+                ) {
                     Text(text = "=")
                 }
 
             }
 
         }
+
     }
 }
